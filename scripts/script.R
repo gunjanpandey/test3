76c71b5b5665e6fcc1473f7b5717bb1691c30392
Pandey <pan081@csiro.au>
2019-05-09 12:48:33 +1000

    12:48 PM before lunch

--- scripts/script.R
+++ scripts/script.R
@@ -1,4 +1,19 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-d_gap <- read.csv("raw_data/gapminder.csv")
+#load data file. R take folder location from its home directly despite of the folder where script sits 
+# Becareful of the package used to load data
+gapminder <- read_csv("raw_data/gapminder.csv")
+
+#to check the head and tail of the dataframe
+head(gapminder)
+tail(gapminder, 20)
+
+# to have a glipse of the dataframe
+glimpse(gapminder)
+
+# to check only one column
+gapminder$country
+
+#unique 
+unique(gapminder$country)
